680a7e1ee78b5be2b28416fd206714ecdec086d9
Hong Minhee <dahlia@stylesha.re>
2011-09-25 23:59:17 -0400

    Added upload_doc command into setup.py script.

--- setup.py
+++ setup.py
@@ -1,14 +1,46 @@
+import os
 import os.path
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+import distutils.cmd
+import tempfile
+import shutil
 from wand.version import VERSION_INFO
 
 
 def readme():
     with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
         return f.read()
+
+
+class upload_doc(distutils.cmd.Command):
+    """Uploads the documentation to GitHub pages."""
+
+    description = __doc__
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        path = tempfile.mkdtemp()
+        build = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                             'build', 'sphinx', 'html')
+        os.chdir(path)
+        os.system('git clone git@github.com:StyleShare/wand.git .')
+        os.system('git checkout gh-pages')
+        os.system('git rm -r .')
+        os.system('touch .nojekyll')
+        os.system('cp -r ' + build + '/* .')
+        os.system('git stage .')
+        os.system('git commit -a -m "Documentation updated."')
+        os.system('git push origin gh-pages')
+        shutil.rmtree(path)
 
 
 setup(name='Wand',
@@ -21,7 +53,7 @@ setup(name='Wand',
       author_email='dahlia' '@' 'stylesha.re',
       maintainer='StyleShare',
       maintainer_email='dev' '@' 'stylesha.re',
-      url='https://styleshare.github.com/wand/',
+      url='http://styleshare.github.com/wand/',
       tests_require=['Attest'],
       test_loader='attest:auto_reporter.test_loader',
       test_suite='wandtests.tests',
@@ -34,5 +66,6 @@ setup(name='Wand',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Multimedia :: Graphics'
-      ])
+      ],
+      cmdclass={'upload_doc': upload_doc})
 
--- wand/version.py
+++ wand/version.py
@@ -3,6 +3,8 @@
 
 """
 
+__all__ = 'VERSION', 'VERSION_INFO'
+
 #: (:class:`tuple`) The version tuple e.g. ``(0, 1, 2)``.
 VERSION = (0, 1, 0)
 
