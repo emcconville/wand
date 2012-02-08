cc845e0c51a8c3e67979dd38cd5cb15f83022304
Simon Pantzare <simon@pewpewlabs.com>
2012-02-08 01:55:57 +0100

    Add metric type defs from ImageMagick docs

--- wand/image.py
+++ wand/image.py
@@ -54,8 +54,17 @@ FILTER_TYPES = ('undefined', 'point', 'box', 'triangle', 'hermite', 'hanning',
                 'hamming', 'blackman', 'gaussian', 'quadratic', 'cubic',
                 'catrom', 'mitchell', 'lanczos', 'bessel', 'sinc')
 
-
 # TODO sp: Documentation.
+#
+# AE     absolute error count, number of different pixels (-fuzz effected)
+# FUZZ   mean color distance
+# MAE    mean absolute error (normalized), average channel error distance
+# MEPP   mean error per pixel (normalized mean error, normalized peak error)
+# MSE    mean error squared, average of the channel error squared
+# NCC    normalized cross correlation
+# PAE    peak absolute (normalize peak absolute)
+# PSNR   peak signal to noise ratio
+# RMSE   root mean squared (normalized root mean squared)
 METRIC_TYPES = ('undefined', 'absolute_error', 'mean_absolute_error', 
                 'mean_error_per_pixel', 'mean_squared_error', 
                 'peak_absolute_error', 'peak_signal_to_noise_ratio',
