6fe47da0d6c079f57a513879b56d41c715571a86
Hong Minhee <minhee@dahlia.kr>
2012-10-28 06:12:31 +0900

    I'm late
    Copyright 2012

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import sys, os, datetime
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Wand'
-copyright = u'2011, Hong Minhee'
+copyright = str(datetime.date.today().year) + u', Hong Minhee'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
