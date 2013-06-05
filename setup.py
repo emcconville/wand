8c3d8f40c2a2e7e255cd199fc63121d5817e9c5c
Hong Minhee <minhee@dahlia.kr>
2013-06-05 13:15:03 +0900

    Remove upload_doc command

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-import distutils.cmd
-import shutil
-import tempfile
 
 from wand.version import VERSION
 
@@ -14,34 +11,6 @@ from wand.version import VERSION
 def readme():
     with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
         return f.read()
-
-
-class upload_doc(distutils.cmd.Command):
-    """Uploads the documentation to GitHub pages."""
-
-    description = __doc__
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        path = tempfile.mkdtemp()
-        build = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                             'build', 'sphinx', 'html')
-        os.chdir(path)
-        os.system('git clone git@github.com:dahlia/wand.git .')
-        os.system('git checkout gh-pages')
-        os.system('git rm -r .')
-        os.system('touch .nojekyll')
-        os.system('cp -r ' + build + '/* .')
-        os.system('git stage .')
-        os.system('git commit -a -m "Documentation updated."')
-        os.system('git push origin gh-pages')
-        shutil.rmtree(path)
 
 
 setup(
@@ -73,5 +42,4 @@ setup(
         'Programming Language :: Python :: Implementation :: Stackless',
         'Topic :: Multimedia :: Graphics'
       ],
-    cmdclass={'upload_doc': upload_doc}
 )
