1eb71fe6723384d75187e09b25d676b26a1f24fe
ziotibia81 <ziotibia1@libero.it>
2015-06-12 16:45:12 +0200

    Added binding to "MagickResampleImage"

--- wand/api.py
+++ wand/api.py
@@ -344,6 +344,10 @@ try:
     library.MagickResizeImage.argtypes = [ctypes.c_void_p, ctypes.c_size_t,
                                           ctypes.c_size_t, ctypes.c_int,
                                           ctypes.c_double]
+                                          
+   library.MagickResampleImage.argtypes = [ctypes.c_void_p, ctypes.c_double,
+                                          ctypes.c_double, ctypes.c_int,
+                                          ctypes.c_double]
 
     library.MagickTransformImage.argtypes = [ctypes.c_void_p, ctypes.c_char_p,
                                              ctypes.c_char_p]
