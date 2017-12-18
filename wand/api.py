e9c7c31a316295cd297cc7734fe41036ba0bbdbf
yoch <yoch.melka@gmail.com>
2017-12-19 00:09:07 +0200

    Update api.py
    Adding MagickThumbnailImage to api

--- wand/api.py
+++ wand/api.py
@@ -425,6 +425,9 @@ try:
                                           ctypes.c_size_t, ctypes.c_int,
                                           ctypes.c_double]
 
+    library.MagickThumbnailImage.argtypes = [ctypes.c_void_p, ctypes.c_size_t,
+                                             ctypes.c_size_t]     
+
     library.MagickTransformImage.argtypes = [ctypes.c_void_p, ctypes.c_char_p,
                                              ctypes.c_char_p]
     library.MagickTransformImage.restype = ctypes.c_void_p
