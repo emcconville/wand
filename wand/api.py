53cf21bc46707c19b31fdbff1fdb4a7c854bfac7
Eric McConville <emcconville@emcconville.com>
2018-11-12 20:05:18 -0600

    Flake8 + fixed broken link

--- wand/api.py
+++ wand/api.py
@@ -424,7 +424,7 @@ try:
     library.MagickResizeImage.argtypes = [ctypes.c_void_p, ctypes.c_size_t,
                                           ctypes.c_size_t, ctypes.c_int,
                                           ctypes.c_double]
-                                          
+
     library.MagickResampleImage.argtypes = [ctypes.c_void_p, ctypes.c_double,
                                             ctypes.c_double, ctypes.c_int,
                                             ctypes.c_double]
