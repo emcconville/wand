7b9fbd97f21870362f7553a92dc11e9357132959
Eric McConville <emcconville@emcconville.com>
2018-11-13 20:51:25 -0600

    Removed README from data_files. Fixes issue #336 and README contents is already included in long_description

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ else:
 setup(
     name='Wand',
     packages=['wand'],
-    data_files=[('', ['README.rst'])],
     version=VERSION,
     description='Ctypes-based simple MagickWand API binding for Python',
     long_description=readme(),
