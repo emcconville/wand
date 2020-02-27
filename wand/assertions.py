44672bbc6f79201851753ddde49f80e501d676bf
E. McConville <emcconville@emcconville.com>
2020-02-26 20:52:59 -0600

    Started building out coordinate assertions method

--- wand/assertions.py
+++ wand/assertions.py
@@ -8,8 +8,53 @@ This module checks user input before calling MagickWands C-API methods.
 """
 
 import numbers
+import collections.abc
 
 from .compat import string_type
+
+
+def assert_bool(**kwargs):
+    """Ensure all given values are boolean.
+
+    :raises TypeError: if value is not ``True`` or ``False.
+
+    .. versionadded:: 0.5.4
+    """
+    for label, subject in kwargs.items():
+        if not isinstance(subject, bool):
+            fmt = "{0} must be a bool, not {1}"
+            msg = fmt.format(label, repr(subject))
+            raise TypeError(msg)
+
+
+def assert_color(**kwargs):
+    """Ensure all given values are instances of :class:`~wand.color.Color`.
+
+    :raises TypeError: if value is not :class:`~wand.color.Color`.
+
+    .. versionadded:: 0.5.4
+    """
+    for label, subject in kwargs.items():
+        if not isinstance(subject, Color):
+            fmt = "Expecting an instance of wand.color.Color for {0}, not {1}"
+            msg = fmt.format(label, repr(subject))
+            raise TypeError(msg)
+
+
+def assert_counting_number(**kwargs):
+    """Ensure all given values are natural integer.
+
+    :raises TypeError: if value is not an integer.
+    :raises ValueError: if value is less than ``1``.
+
+    .. versionadded:: 0.5.4
+    """
+    assert_integer(**kwargs)
+    for label, subject in kwargs.items():
+        if subject < 1:
+            fmt = "{0}={1} must be an natural number greater than 0"
+            msg = fmt.format(label, subject)
+            raise ValueError(msg)
 
 
 def assert_integer(**kwargs):
@@ -41,7 +86,7 @@ def assert_real(**kwargs):
 
 
 def assert_unsigned_integer(**kwargs):
-    """Ensure all given values are postive integer.
+    """Ensure all given values are positive integer.
 
     :raises TypeError: if value is not an integer.
     :raises ValueError: if value is less than ``0``.
@@ -51,38 +96,34 @@ def assert_unsigned_integer(**kwargs):
     assert_integer(**kwargs)
     for label, subject in kwargs.items():
         if subject < 0:
-            fmt = "{0}={1} must be a postive integer"
+            fmt = "{0}={1} must be a positive integer"
             msg = fmt.format(label, subject)
             raise ValueError(msg)
 
 
-def assert_counting_number(**kwargs):
-    """Ensure all given values are natural integer.
+def assert_coordinate(**kwargs):
+    """Ensure all given values are a sequence of 2 real numbers.
 
-    :raises TypeError: if value is not an integer.
-    :raises ValueError: if value is less than ``1``.
+    :raises TypeError: if value is not a pair of doubles.
 
-    .. versionadded:: 0.5.4
-    """
-    assert_integer(**kwargs)
-    for label, subject in kwargs.items():
-        if subject < 1:
-            fmt = "{0}={1} must be an natural number greater than 0"
-            msg = fmt.format(label, subject)
-            raise ValueError(msg)
-
-
-def assert_bool(**kwargs):
-    """Ensure all given values are boolean.
-
-    :raises TypeError: if value is not ``True`` or ``False.
-
-    .. versionadded:: 0.5.4
+    .. versionadded:: 0.6.0
     """
     for label, subject in kwargs.items():
-        if not isinstance(subject, bool):
-            fmt = "{0} must be a bool, not {1}"
+        if not isinstance(subject, collections.abc.Sequence):
+            fmt = "'{0}' must be a pair of real numbers, not {1}"
             msg = fmt.format(label, repr(subject))
+            raise TypeError(msg)
+        elif len(subject) != 2:
+            fmt = "'{0}' must be a exactly 2 real numbers, not {1}"
+            msg = fmt.format(label, len(subject))
+            raise TypeError(msg)
+        elif not isinstance(subject[0], numbers.Real):
+            fmt = "first entry of '{0}' must be a real number, not {1}"
+            msg = fmt.format(label, repr(subject[0]))
+            raise TypeError(msg)
+        elif not isinstance(subject[1], numbers.Real):
+            fmt = "second entry of '{0}' must be a real number, not {1}"
+            msg = fmt.format(label, repr(subject[1]))
             raise TypeError(msg)
 
 
@@ -90,20 +131,6 @@ def assert_string(**kwargs):
     for label, subject in kwargs.items():
         if not isinstance(subject, string_type):
             fmt = "{0} must be a string, not {1}"
-            msg = fmt.format(label, repr(subject))
-            raise TypeError(msg)
-
-
-def assert_color(**kwargs):
-    """Ensure all given values are instances of :class:`~wand.color.Color`.
-
-    :raises TypeError: if value is not :class:`~wand.color.Color`.
-
-    .. versionadded:: 0.5.4
-    """
-    for label, subject in kwargs.items():
-        if not isinstance(subject, Color):
-            fmt = "Expecting an instance of wand.color.Color for {0}, not {1}"
             msg = fmt.format(label, repr(subject))
             raise TypeError(msg)
 
