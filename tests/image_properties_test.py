f781202bafb88302390fd2f33f476763b4b5fcca
E. McConville <emcconville@emcconville.com>
2021-02-10 14:33:00 -0600

    Using PNM format for resolution test (replacing SVG attempt) #519

--- tests/image_properties_test.py
+++ tests/image_properties_test.py
@@ -539,19 +539,15 @@ def test_resolution_set_02(fx_asset):
         assert img.resolution == (100, 100)
 
 
-def test_resolution_set_03(fx_asset):
+def test_resolution_set_03():
     """Sets image resolution on constructor"""
-    with Image(filename='msvg:' + str(fx_asset.join('svg_logo.svg')),
-                resolution=(100, 100),
-                width=94, height=34) as img:
+    with Image(filename='rose:', resolution=(100, 100)) as img:
         assert img.resolution == (100, 100)
 
 
-def test_resolution_set_04(fx_asset):
+def test_resolution_set_04():
     """Sets image resolution on constructor with integer as parameter."""
-    with Image(filename='msvg:' + str(fx_asset.join('svg_logo.svg')),
-                resolution=100,
-                width=94, height=34) as img:
+    with Image(filename='rose:', resolution=100) as img:
         assert img.resolution == (100, 100)
 
 
